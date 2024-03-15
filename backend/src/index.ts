dfd84f1fda5e31c796e7db11bf29f5bf07cbb17e
Aviral Vardiya <aviralvardiya10@gmail.com>
2024-03-15 21:21:04 +0530

    root api response added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,98 +15,12 @@ const app = new Hono<{
   };
 }>();
 
+app.get("/",(c)=>{
+  return c.text("welcome to blog app backend, this backed is deployed on cloudflare workers and it is a serverless deployment")
+})
+
 app.route("/api/v1/user",userRoute)
 
-// app.post("/api/v1/user/signup", async (c) => {
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env.DATABASE_URL,
-//   }).$extends(withAccelerate());
-//   const { email, password, name } = await c.req.json();
-//   try {
-//     const newUser = await prisma.user.create({
-//       data: { email, password, name },
-//     });
-//     const token = await sign(
-//       { id: newUser.id, email: newUser.email },
-//       c.env.JWT_SECRET
-//     );
-//     return c.json({ msg: "user created", token: token });
-//   } catch (error) {
-//     return c.status(403);
-//   }
-// });
-
-// app.post("/api/v1/user/signin", async (c) => {
-//   const { email, password } = await c.req.json();
-
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env.DATABASE_URL,
-//   }).$extends(withAccelerate());
-//   try {
-//     const fetchedUser = await prisma.user.findUnique({
-//       where: { email: email },
-//     });
-//     if (fetchedUser) {
-//       if (fetchedUser.password === password) {
-//         const token = await sign(
-//           { id: fetchedUser.id, email: fetchedUser.email },
-//           c.env.JWT_SECRET
-//         );
-//         return c.json({ msg: "logged in successfully", token: token });
-//       } else {
-//         return c.json({ msg: "invalid creds" });
-//       }
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return c.json("internal error");
-//   }
-// });
-
 app.route("/api/v1/blog",blogRoute)
-
-// app.use("/api/v1/blog/*", async (c, next) => {
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env.DATABASE_URL,
-//   }).$extends(withAccelerate());
-
-//   console.log("blog middleware fired");
-//   const authHeader = await c.req.header("authorization");
-//   if (authHeader) {
-//     const token = authHeader.split(" ")[1];
-//     const info = await verify(token, c.env.JWT_SECRET);
-//     const fetchedUser = await prisma.user.findUnique({
-//       where: {
-//         email: info.email,
-//       },
-//     });
-//     if (fetchedUser) {
-//       c.set("userId", info.id);
-//       await next();
-//     } else {
-//       return c.notFound();
-//     }
-//   } else {
-//     return c.notFound();
-//   }
-// });
-
-// app.post("/api/v1/blog", (c) => {
-//   return c.text("Hello Hono!");
-// });
-
-// app.put("/api/v1/blog", (c) => {
-//   return c.text("Hello Hono!");
-// });
-
-// app.get("/api/v1/blog/:id", (c) => {
-// // console.log(c.get("userId"));
-
-//   return c.text("Hello Hono!");
-// });
-
-// app.get("/api/v1/blog/bulk", (c) => {
-//   return c.text("Hello Hono!");
-// });
 
 export default app;
