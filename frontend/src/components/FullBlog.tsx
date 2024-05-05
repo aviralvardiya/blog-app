f20f0929af2788373fa860c133d17902c0dd79b3
Aviral Vardiya <aviralvardiya10@gmail.com>
2024-05-05 11:21:58 +0530

    changed mobile style for full blog page

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -6,8 +6,8 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
     return <div>
         <Appbar />
         <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-8">
+            <div className="flex flex-col lg:grid lg:grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+                <div className="lg:col-span-8 mb-10">
                     <div className="text-5xl font-extrabold">
                         {blog.title}
                     </div>
@@ -18,7 +18,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className="col-span-4">
+                <div className="lg:col-span-4">
                     <div className="text-slate-600 text-lg">
                         Author
                     </div>
